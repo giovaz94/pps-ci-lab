ccb984c5fba267f4bc4e538a5b778c15cfbddedf
Lorenzo Campanelli <lorenzo.campanelli3@studio.unibo.it>
2023-05-02 17:34:21 +0200

    Refactor Main class

--- src/main/java/LorenzoMain.java
+++ src/main/java/LorenzoMain.java
@@ -1,3 +1,6 @@
+/**
+ * This class greets everyone who runs the program with a fancy message.
+ */
 public class LorenzoMain {
     public static void main(String[] args) {
         System.out.println("Hello World by Lorenzo!");
